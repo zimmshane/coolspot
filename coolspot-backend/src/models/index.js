982d9b45a6810c98097d207432d818d6e0872ac8
zimmshane <zimmshane@gmail.com>
2025-08-21 14:55:29 -0700

    add api search

--- coolspot-backend/src/models/index.js
+++ coolspot-backend/src/models/index.js
@@ -1,20 +1,21 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const { MongoClient } = require('mongodb'); // Import the MongoClient
 
 
 const MONGODB_URI = "NONE"
-const User = require('./user');
 const Spot = require('./spot');
-const SpotLike = require('./spotLike');
-const SpotVisit = require('./SpotVisit');
-const Comment = require('./Comment');
+
 
 const app = express();
 const port = 3001;
 
 const client = new MongoClient(MONGODB_URI);
 const dbName = 'spotsDB';
+
+app.use(cors());
+app.use(cookieParser());
 
 async function connectToDb() {
   try {
@@ -27,7 +28,54 @@ async function connectToDb() {
   }
 }
 
+
 let db = connectToDb();
+
+app.get('/', (req, res) => {
+    let visitCount = parseInt(req.cookies.visitCount) || 0;
+    visitCount++;
+
+    // Set the cookie in the user's browser
+    res.cookie('visitCount', visitCount.toString(), { maxAge: 900000, httpOnly: true });
+
+    res.send(`Welcome! You have visited this page ${visitCount} times.`);
+});
+
+app.get('/search', async (req, res) => {
+    const nameQuery = req.query.name;
+    if (!nameQuery) {
+        return res.status(400).send('A name query parameter is required.');
+    }
+    try {
+        const results = await db.collection('spots').find({
+            name: { $regex: nameQuery, $options: 'i' }
+        }).toArray();
+        res.json(results);
+    } catch (err) {
+        res.status(500).send('Error during search');
+    }
+});
+
+app.get('/spots/filter', async (req, res) => {
+    const { genre, publishedAfter } = req.query;
+    let filter = {};
+
+    // Build the filter object dynamically based on query parameters
+    if (genre) {
+        filter.genre = genre;
+    }
+    if (publishedAfter) {
+        // Use $gt for "greater than"
+        filter.year = { $gt: parseInt(publishedAfter) };
+    }
+
+    try {
+        const results = await db.collection('books').find(filter).toArray();
+        res.json(results);
+    } catch (err) {
+        res.status(500).send('Error applying filters');
+    }
+});
 
 
 
